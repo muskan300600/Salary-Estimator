751b9991ef61fa2d6f02e21421ccac1158a33897
Muskan <imh10017.18@bitmesra.ac.in>
2021-04-17 00:09:50 +0530

    Data Cleaning file added

---
+++ data_cleaning.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('glassdoor_jobs.csv')
+
+df = df[df['Salary Estimate'] != '-1']
+salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
+minus_Kd = salary.apply(lambda x: x.replace('K','').replace('$',''))
+
+df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
+
